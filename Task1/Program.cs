8992caa62e7ca7997764c31cfcab6cdc3175739b
esarvas <esarvas@WS-SARVAS>
2017-04-26 11:01:20 +0300

    Added MainMenu

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -16,7 +16,7 @@ namespace Task1
 
             CandySugar Candy1 = new CandySugar("Candy1",15,25);
             CandySugarSkin Candy2 = new CandySugarSkin("Candy2", 10, 20, "Глазурь");
-            CandySugarSkinStuffing Candy3 = new CandySugarSkinStuffing("Candy3", 20, 30, "Глазурь", "Орехи");
+            CandySugarSkinStuffing Candy3 = new CandySugarSkinStuffing("Candy3", 20, 25, "Глазурь", "Орехи");
             CandyWithoutSugar Candy4 = new CandyWithoutSugar("Candy4", 12);
             CandyWithoutSugarSkin Candy5 = new CandyWithoutSugarSkin("Candy5", 18, "Шоколад");
            
@@ -32,32 +32,89 @@ namespace Task1
             string temp_sugar_string;
             double temp_sugar_double = 0;
             double temp_weight = 0;
+            int z_count = 0;
+            string menu_command = "";
+            int check_double_flag = 0;
 
-            Console.WriteLine("Введите содержание сахара для поиска конфеты с аналогичными параметрами");
-            temp_sugar_string = Console.ReadLine();
+                        
+            do {
+                Console.WriteLine("1. Что бы вывести все конфеты подарка введите 1 ");
+                Console.WriteLine("2. Что бы найти конфету по задданому показателю сахара введите 2 ");
+                Console.WriteLine("3. Что бы упорядочить подарок по одному из показателей введите 3 ");
+                Console.WriteLine("4. Что бы узнать вес подарка введите 4 ");
+                Console.WriteLine("5. Что бы закончить работу введите 5 ");
+                menu_command = Console.ReadLine(); 
 
-            try
-            { temp_sugar_double = double.Parse(temp_sugar_string); }
-            catch { Console.WriteLine("Введите корректное число"); }
+                switch (menu_command)
+                {
+                    case "1" :
 
-           // Console.WriteLine(temp_sugar_double);
+                        foreach (var i in Present)
 
-            foreach (var i in Present)
-
-            {
-                 // Console.WriteLine(i.CandyName); Console.ReadKey();
-
-            if (i is ISugarConcentration) { temp_weight = temp_weight + i.CandyWeight; };
-                             
-
-               
-            }
-            
-       
+                        {
+                           var sugar_temp = i as ISugarConcentration;
+                           var skin_temp = i as ISkin;
+                           var stuffing_temp = i as IStuffing;
+                            Console.Write("Название конфеты {0}, вес конфеты {1}", i.CandyName, i.CandyWeight);
+                            if (sugar_temp != null) { Console.Write(", содержание сахара {0}", sugar_temp.SugarPerThing ); };
+                            if (skin_temp != null) { Console.Write(", оболочка {0}", skin_temp.Skin); };
+                            if (stuffing_temp != null) { Console.Write(", начинка {0}", stuffing_temp.Stuffing); };
+                            Console.WriteLine();
+                        }
 
 
-            Console.WriteLine("Вес подарка {0} ", temp_weight);
-            Console.ReadKey();
+                        break;
+
+                    case "2" :
+                        
+                        do
+                        {
+                            Console.WriteLine("Введите содержание сахара для поиска конфеты с аналогичными параметрами");
+                            temp_sugar_string = Console.ReadLine();
+
+                            if (double.TryParse (temp_sugar_string, out temp_sugar_double) ) { check_double_flag = 0; }
+                            else { Console.WriteLine("Введите корректное число"); check_double_flag = 1; };
+                        }
+                        while (check_double_flag == 1);
+                       
+                        
+                        foreach (var i in Present)
+                        {
+                            var z = i as ISugarConcentration;
+                            if (z != null) { if (z.SugarPerThing == temp_sugar_double) { Console.WriteLine("{0} сахара содержится в конфете {1}", temp_sugar_double, i.CandyName); z_count++; } };
+                        }
+                        if (z_count == 0) { Console.WriteLine("В подарке нет конфет с содержанием сахара {0}", temp_sugar_double); };
+                        z_count = 0;
+                        Console.WriteLine();
+
+                        break;
+
+
+                    case "3" :
+
+                        break;
+
+                    case "4" :
+
+                        foreach (var i in Present)
+                        {
+                            if (i is ICandyBasic) { temp_weight = temp_weight + i.CandyWeight; };
+             
+                        }
+                        Console.WriteLine("Вес подарка {0} ", temp_weight);
+                        Console.WriteLine();
+                        
+                        break;
+
+                };
+
+             }
+            while (menu_command != "5");
+
+
+
+
+
         }
            
     }
